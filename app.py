cd12cd965d7f069c70e54104890132595618f12b
Suraj Karan Vejandla <110095400+surajkaran69@users.noreply.github.com>
2023-04-25 09:36:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,43 @@
-# Import libraries and modules
-
-from flask import Flask, render_template, request
-import numpy as np
-from keras.models import load_model
+import streamlit as st
+import tensorflow as tf
 import librosa
-import os
+import numpy as np
 
+# Load the trained model
+model = tf.keras.models.load_model('C:/Users/suraj/Downloads/speechemotionrecognition.h5')
 
-app = Flask(__name__)
+# Define the function to recognize speech
+def recognize_speech(audio_file):
+    # Load the audio file using librosa
+    audio, sr = librosa.load(audio_file, duration=3, offset=0.5)
 
-# Loading crop recommendation model
-
-model = load_model('speechemotionrecognition.h5')
-
-
-
-# render home page
-
-@ app.route('/')
-def home():
-    return render_template('index.html')
-
-@app.route('/predict', methods = ['POST'])
-def predict():
+    # Reshape the audio file to match the model input shape
+    audio = np.mean(librosa.feature.mfcc(y=audio, sr=sr, n_mfcc=40).T, axis=0)
+    audio = audio.reshape(1, -1)
     
-    if request.method == 'POST':
-        f = request.files["file"]
-        y, sr = librosa.load(f, duration = 3, offset=0.5)
-        mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T, axis=0)
-        mfcc = mfcc.reshape(1, -1)
-
-        prediction = model.predict([mfcc])
-        prediction=np.argmax(prediction,axis=1)
-        output = prediction[0]
-        
-        if output == 0:
-            return render_template('index.html', prediction='Speech Emotion is Angry')
-        
-        elif output == 1:
-            return render_template('index.html', prediction='Speech Emotion is Disgust')
-        
-        elif output == 2:
-            return render_template('index.html', prediction='Speech Emotion is Fear')
-        
-        elif output == 3:
-            return render_template('index.html', prediction='Speech Emotion is Happy')
-        
-        elif output == 4:
-            return render_template('index.html', prediction='Speech Emotion is Neutral')
-        
-        elif output == 5:
-            return render_template('index.html', prediction='Speech Emotion is Surprise')
-        
-        elif output == 6:
-            return render_template('index.html', prediction='Speech Emotion is Sad')
-   
+    # Use the model to recognize the speech
+    predicted_label = model.predict(audio)
+    predicted_label = np.argmax(predicted_label, axis=1)[0]
     
+    # Map the predicted label to the corresponding emotion string
+    emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Surprise', 'Sad']
+    predicted_emotion = emotions[predicted_label]
+    
+    return f'Speech Emotion is {predicted_emotion}'
 
+# Define the Streamlit app
+def app():
+    st.title('Speech Recognition App')
+    
+    # Add a file uploader to let users upload the audio file
+    uploaded_file = st.file_uploader('Upload an audio file', type=['wav','mp3'])
+    
+    if uploaded_file is not None:
+        # Call the recognize_speech function to recognize the speech in the uploaded file
+        predicted_label = recognize_speech(uploaded_file)
+        # Show the predicted label to the user
+        st.write(predicted_label)
 
+# Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app()
