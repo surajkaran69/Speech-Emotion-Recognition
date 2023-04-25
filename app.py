dfd97f88cf2686a995ab06f41400bbb448f9b83c
Suraj Karan Vejandla <110095400+surajkaran69@users.noreply.github.com>
2023-04-25 10:35:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import tensorflow as tf
 import librosa
 import numpy as np
+from pydub import AudioSegment
+import io
+import os
 
 # Load the trained model
 model = tf.keras.models.load_model('speechemotionrecognition.h5')
@@ -25,21 +28,32 @@ def recognize_speech(audio_file):
     
     return f'Speech Emotion is {predicted_emotion}'
 
+# Define the get_file_ext function
+def get_file_ext(file_name):
+    root, ext = os.path.splitext(file_name)
+    return ext[1:]
+
 # Define the Streamlit app
 def app():
-    st.title('Speech Emotion Recognition App')
-    st.sidebar.markdown('Connect with me on LinkedIn:')
-    st.sidebar.markdown('[My LinkedIn ID](https://www.linkedin.com/in/surajkaran/)')
-    
+    st.title('Speech Recognition App')
     # Add a file uploader to let users upload the audio file
     uploaded_file = st.file_uploader('Upload an audio file', type=['wav','mp3'])
-    
     if uploaded_file is not None:
+        # Convert the audio file to a playable format
+        audio_bytes = uploaded_file.read()
+        audio_segment = AudioSegment.from_file(io.BytesIO(audio_bytes), format=get_file_ext(uploaded_file.name))
+        audio_wav = audio_segment.export(format="wav")
+        audio_file = io.BytesIO(audio_wav.read())
+
+        # Display the audio player
+        st.audio(audio_bytes)
+
         # Call the recognize_speech function to recognize the speech in the uploaded file
-        predicted_label = recognize_speech(uploaded_file)
+        predicted_label = recognize_speech(audio_file)
         # Show the predicted label to the user
-        st.write(predicted_label)
+        st.write('Predicted label:', predicted_label)
 
 # Run the app
 if __name__ == '__main__':
     app()
+
